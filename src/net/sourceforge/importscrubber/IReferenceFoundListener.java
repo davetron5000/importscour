680906de179b77318a43aa9d2bfcad87b285f0de
tomcopeland <tomcopeland>
2002-03-18 02:21:22 +0000

    Removing old files

--- src/net/sourceforge/importscrubber/IReferenceFoundListener.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.importscrubber;
-
-/**
- * This interface defines the behavior of something that wants to be notified when
- * new class references are found
- */
-public interface IReferenceFoundListener {
-	public void referenceFound(String className);
-}
