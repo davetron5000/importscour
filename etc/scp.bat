8604a26bf2e49697e7a377ef9452dac2dafeea8f
tomcopeland <tomcopeland>
2002-04-19 20:32:15 +0000

    Changed drive from d to c

--- etc/scp.bat
+++ etc/scp.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set ROOT=d:\data\importscrubber\importscrubber\
+set ROOT=c:\data\importscrubber\importscrubber\
 
 set classpath=%ROOT%lib\bcel.jar
 set classpath=%CLASSPATH%;%ROOT%lib\junit.jar
@@ -8,4 +8,4 @@ set classpath=%CLASSPATH%;%ROOT%lib\optional.jar
 set classpath=%CLASSPATH%;%ROOT%build;
 set classpath=%CLASSPATH%;%ROOT%etc;
 
-set path=%path%;d:\jdk1.3.1_03\bin
+set path=%path%;c:\jdk1.3.1_03\bin
