18e6468e1275205a36eaf1c4ddf0a5eab4f4cde3
ellisj <ellisj>
2002-11-29 18:11:33 +0000

    add new files required by last

---
+++ src/net/sourceforge/importscrubber/FileChooser.java
@@ -0,0 +1,110 @@
+package net.sourceforge.importscrubber;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+// note: could have used threads rather than the iterator approach used here,
+// but that would have bought us nothing in the command line version (we don't WANT
+// a thread scrubbing while this finds files, because most disk systems handle
+// that kind of load very poorly).  so since our only concern is allowing the GUI
+// to update as files are found, this is just as effective -- and simpler.
+
+public class FileChooser implements Iterator
+{
+    private static ResourceBundle res = ResourceBundle.getBundle("net.sourceforge.importscrubber.Resources");
+    private String _sourceRoot, _classRoot;
+    private boolean _recurse;
+    // pre-computed for efficiency
+    int relStart;
+
+    private LinkedList possibles = new LinkedList();
+    private FilePair nextFp;
+
+    public FileChooser(String sourceRoot, String classRoot, boolean recurse) throws IOException
+    {
+        File file = new File(sourceRoot);
+        if (!file.exists())
+            throw new IllegalArgumentException(Resources.ERR_DIR_NOT_EXIST);
+        _sourceRoot = file.getCanonicalPath();
+
+        file = new File(classRoot);
+        if (!file.isDirectory())
+            throw new IllegalArgumentException(Resources.ERR_NOT_DIR);
+        if (!file.exists())
+            throw new IllegalArgumentException(Resources.ERR_DIR_NOT_EXIST);
+        _classRoot = file.getCanonicalPath();
+
+        _recurse = recurse;
+
+        relStart = ImportScrubber.getDirectory(_sourceRoot).length();
+
+        possibles.add(_sourceRoot);
+    }
+
+    public void remove
+        ()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean hasNext()
+    {
+        if (nextFp == null) {
+            try {
+                nextFp = (FilePair)next();
+            } catch (NoSuchElementException e) {}
+        }
+        return nextFp != null;
+    }
+
+    public Object next() throws NoSuchElementException
+    {
+        if (nextFp != null) {
+            FilePair f = nextFp;
+            nextFp = null;
+            return f;
+        }
+
+        FilePair fp = null;
+        while (fp == null) {
+            fp = nextPossible();
+        }
+        return fp;
+    }
+
+    // returns null if the sourcefile didn't have a matching classfile.
+    private FilePair nextPossible() throws NoSuchElementException
+    {
+        String s = (String)possibles.removeFirst();
+        File tmp = new File(s);
+        if (tmp.isDirectory()) {
+            if (_recurse) {
+                String[] entries = tmp.list(new JavaFileFilter());
+                for (int i = 0; i < entries.length; i++) {
+                    possibles.add(s + ImportScrubber.FILE_SEPARATOR + entries[i]);
+                }
+            }
+            return nextPossible();
+        } else {
+            return getPair(tmp);
+        }
+    }
+
+    private FilePair getPair(File source)
+    {
+        if (!(source.canRead() && source.canWrite())) {
+            return null;
+        }
+        String relativeSourceFilename = source.getParent().substring(relStart);
+
+        String cfilename = source.getName().substring(0, source.getName().length() - 5) + ".class";
+        String classfilename = _classRoot + relativeSourceFilename + ImportScrubber.FILE_SEPARATOR + cfilename;
+        File classfile = new File(classfilename);
+        if(classfile.exists()) {
+            return new FilePair(source, classfile);
+        }
+
+        return null;
+    }
+}
---
+++ src/net/sourceforge/importscrubber/StatementFormat.java
@@ -0,0 +1,125 @@
+package net.sourceforge.importscrubber;
+
+import java.util.*;
+
+public class StatementFormat
+{
+    public static final int BREAK_EACH_PACKAGE = 0;
+    public static final int BREAK_NONE = 1;
+
+    private static ArrayList _formats = new ArrayList(2);
+
+    static {
+        ResourceBundle res = ResourceBundle.getBundle("net.sourceforge.importscrubber.Resources");
+        _formats.add(res.getString(Resources.BREAK_EACH_PACKAGE));
+        _formats.add(res.getString(Resources.BREAK_NONE));
+    }
+
+    public static Object keyToValue(int key)
+    {
+        return _formats.get(key);
+    }
+    public static int valueToKey(Object value)
+    {
+        return _formats.indexOf(value);
+    }
+
+    public static Iterator formatIterator()
+    {
+        return _formats.iterator();
+    }
+
+    // get a description of args understood by getFormat
+    public static String getUsage()
+    {
+        return "-sortJavaLibsHigh: place 'import java...' statements at the top [default: off]\n"
+               + "-combineThreshold <integer>: import * from a package when MORE THAN this number of imports are found [default: 0 = off]\n"
+               + "-thresholdStandardOnly: apply combineThreshold only to java standard library; other packages will continue to import individual classes no matter how many are used [default: off]\n"
+               + "-breakEachPackage: linebreak after the imports from each package [default: off]\n";
+    }
+
+    // arg parsing done here to make it easier for others to add new features
+    public static StatementFormat getFormat(String[] args)
+    {
+        int breakStyle = BREAK_NONE;
+        int combineThreshold = 0;
+        boolean sortJavaLibsHigh = ImportScrubber.argExists("sortjavalibshigh", args);
+        boolean thresholdStandardOnly = ImportScrubber.argExists("thresholdStandardOnly", args);
+        if (ImportScrubber.argExists("combineThreshold", args)) {
+            combineThreshold = new Integer(ImportScrubber.findArg("combineThreshold", args)).intValue();
+        }
+        if (ImportScrubber.argExists("breakEachPackage", args)) {
+            breakStyle = BREAK_EACH_PACKAGE;
+        }
+
+        return new StatementFormat(sortJavaLibsHigh, breakStyle, combineThreshold, thresholdStandardOnly);
+    }
+
+    //
+    // the actual format stuff
+
+    private boolean _sortJavaLibsHigh;
+    private int _breakStyle;
+    private int _combineThreshold;
+    private boolean _thresholdStandardOnly; // true if threshold should only apply to standard libraries
+    private ArrayList identical; // for applyFormat -- stored here so we avoid re-allocating each time
+
+    public StatementFormat(boolean sortJavaLibsHigh, int breakStyle, int combineThreshold, boolean thresholdStandardOnly)
+    {
+        _sortJavaLibsHigh = sortJavaLibsHigh;
+        _breakStyle = breakStyle;
+        _combineThreshold = combineThreshold;
+        _thresholdStandardOnly = thresholdStandardOnly;
+        identical = new ArrayList(_combineThreshold);
+    }
+
+    public StringBuffer applyFormat(List list)
+    {
+        Collections.sort(list, new ImportStatementComparator(_sortJavaLibsHigh));
+
+        // pre-process the threshold requirement
+        if (_combineThreshold > 0) {
+            List oldList = list;
+            list = new ArrayList();
+            String lastPackage = null;
+            identical.clear();
+            for (Iterator i = oldList.iterator(); i.hasNext(); ) {
+                ImportStatement stmt = (ImportStatement)i.next();
+                if (lastPackage == null || lastPackage.compareTo(stmt.getPackage()) != 0) {
+                    if (identical.size() > _combineThreshold) {
+                        list.add(new ImportStatement(lastPackage + ".*"));
+                    } else {
+                        list.addAll(identical);
+                    }
+                    identical.clear();
+                    lastPackage = stmt.getPackage();
+                }
+                if (_thresholdStandardOnly
+                        && !(stmt.isInStdJavaLibrary() || stmt.isInStdJavaExtensionLibrary())) {
+                    list.add(stmt);
+                } else {
+                    identical.add(stmt);
+                }
+            }
+            if (identical.size() > _combineThreshold) {
+                list.add(new ImportStatement(lastPackage + ".*"));
+            } else {
+                list.addAll(identical);
+            }
+        }
+
+        StringBuffer result = new StringBuffer();
+        ImportStatement last = null;
+        for (Iterator i = list.iterator(); i.hasNext(); ) {
+            ImportStatement next = (ImportStatement)i.next();
+            if (_breakStyle == BREAK_EACH_PACKAGE) {
+                if (last != null && !last.getPackage().equals(next.getPackage())) {
+                    result.append(ImportScrubber.LINE_SEPARATOR);
+                }
+            }
+            last = next;
+            result.append(next.getFormattedStmt());
+        }
+        return result;
+    }
+}
