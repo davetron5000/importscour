11c286b91ace2cff66195f54a2464c606f7dde6b
David Copeland <davetron5000@gmail.com>
2008-12-08 23:54:36 -0500

    Added annotations (seems to work)

--- test/test_project/src/java/net/sourceforge/importscrubber/FunctionalTest.java
+++ test/test_project/src/java/net/sourceforge/importscrubber/FunctionalTest.java
@@ -14,6 +14,7 @@ import java.util.*;
 import java.util.Map.Entry;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import org.testng.annotations.*;
 
 import static java.lang.Math.*;
 
@@ -85,6 +86,7 @@ public class FunctionalTest implements Runnable {
       return null;
    }
 
+   @Test
    public Component getComp() {
       return null;
    }
